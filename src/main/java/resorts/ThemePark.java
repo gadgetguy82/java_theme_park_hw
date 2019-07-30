122428e2ed9fc1d2e78afaf20384b45b1a226199
Robert Tam <rsh.tam@gmail.com>
2019-07-30 19:09:27 +0100

    Add ThemeParkTest and ThemePark classes to project

---
+++ src/main/java/resorts/ThemePark.java
@@ -0,0 +1,4 @@
+package resorts;
+
+public class ThemePark {
+}
---
+++ src/test/java/resorts/ThemeParkTest.java
@@ -0,0 +1,12 @@
+package resorts;
+
+import org.junit.Before;
+
+public class ThemeParkTest {
+  ThemePark themePark;
+
+  @Before
+  public void setUp() {
+    themePark = new ThemePark();
+  }
+}
