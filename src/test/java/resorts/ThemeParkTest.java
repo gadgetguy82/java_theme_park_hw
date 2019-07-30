ecf1d6081e65727ed24618be2fcc2d9f9fe34dc9
Robert Tam <rsh.tam@gmail.com>
2019-07-30 19:18:15 +0100

    Add setUp for theme park tests

--- src/test/java/resorts/ThemeParkTest.java
+++ src/test/java/resorts/ThemeParkTest.java
@@ -1,12 +1,34 @@
 package resorts;
 
+import attractions.*;
 import org.junit.Before;
+import org.junit.Test;
+import stalls.*;
 
 public class ThemeParkTest {
   ThemePark themePark;
+  Dodgems dodgems;
+  Park park;
+  Playground playground;
+  RollerCoaster rollerCoaster;
+  CandyflossStall candyflossStall;
+  IceCreamStall iceCreamStall;
+  TobaccoStall tobaccoStall;
 
   @Before
   public void setUp() {
     themePark = new ThemePark();
+    dodgems = new Dodgems("Bumper Cars", 5);
+    park = new Park("Leafy Meadows", 9);
+    playground = new Playground("Fun Zone", 7);
+    rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+    candyflossStall = new CandyflossStall("Candy Land", "Harry Belafonte", ParkingSpot.A1, 8);
+    iceCreamStall = new IceCreamStall("Dream Cones", "Vanilla Ice", ParkingSpot.A4, 7);
+    tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 7);
+  }
+
+  @Test
+  public void hasListOfAttractionsAndStores() {
+
   }
 }
