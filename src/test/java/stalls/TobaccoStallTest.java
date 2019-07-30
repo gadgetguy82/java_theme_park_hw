0bdf954651b2a4b9f834d1dc8e804b4c57358903
Robert Tam <rsh.tam@gmail.com>
2019-07-30 17:41:30 +0100

    Add tests for implementing ISecurity in TobaccoStall

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,20 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1, visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(10, 1.2, 50.00);
+        visitor2 = new Visitor(20, 1.68, 250.00);
     }
 
     @Test
@@ -27,5 +31,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canReturnFalseWhenVisitorUnder18() {
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void canReturnTrueWhenVisitorOver17() {
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor2));
     }
 }
